afcf429003aa1268cddc1aac94cd8ecdf685800e
maximzip <maksymchaikamc@gmail.com>
2026-09-10 19:57:47 +0200

    presentación

--- app.py
+++ app.py
@@ -50,27 +50,27 @@ def predict_get():
 # 3er ENDPOINT. POST con JSON de datos plantilla (se despliega en la presentación)
 # El cliente sube un archivo JSON con los datos de cada trabajador de su plantilla con cada variable 
 # sería como una lista como la siguiente: [{...trabajador1...}, {...trabajador2...}]
-# @app.route("/predict_batch", methods=["POST"])
-# def predict_batch():
-#     lista_empleados = request.get_json()
-#     if not isinstance(lista_empleados, list):
-#         return jsonify({"error": "Se espera una lista de empleados"}), 400
+@app.route("/predict_batch", methods=["POST"])
+def predict_batch():
+    lista_empleados = request.get_json()
+    if not isinstance(lista_empleados, list):
+        return jsonify({"error": "Se espera una lista de empleados"}), 400
 
-#     validos = []
-#     errores = []
-#     for emp in lista_empleados:
-#         resultado, status = _procesar(emp)
-#         (validos if status == 200 else errores).append(resultado)
+    validos = []
+    errores = []
+    for emp in lista_empleados:
+        resultado, status = _procesar(emp)
+        (validos if status == 200 else errores).append(resultado)
 
-#     no_usa_ia = sum(1 for r in validos if r.get("prediction") == 0)
+    no_usa_ia = sum(1 for r in validos if r.get("prediction") == 0)
 
-#     return jsonify({
-#         "total_empleados": len(lista_empleados),
-#         "procesados_correctamente": len(validos),
-#         "con_error": len(errores),
-#         "no_usan_ia": no_usa_ia,
-#         "porcentaje": round(no_usa_ia / len(validos) * 100, 1) if validos else 0
-#     })
+    return jsonify({
+        "total_empleados": len(lista_empleados),
+        "procesados_correctamente": len(validos),
+        "con_error": len(errores),
+        "no_usan_ia": no_usa_ia,
+        "porcentaje": round(no_usa_ia / len(validos) * 100, 1) if validos else 0
+    })
 
 
 if __name__ == "__main__":
